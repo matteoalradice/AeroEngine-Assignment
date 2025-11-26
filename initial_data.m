21e8a358bf68313edba16ac3364f816b7703bc43
jonas <jonasstonealv@gmail.com>
2025-11-26 17:33:45 +0100

    dsadas

--- initial_data.m
+++ initial_data.m
@@ -10,104 +10,50 @@ function engine = initial_data(engineID)
         % =================================================================
         % CASE 1: JT 8D
         % =================================================================
-        case {'jt8d', 'jt 8d'}
-            engine.name      = 'JT 8D';
+        case {'gtf'}
+            engine.name      = 'GTF';
             
             % -- Intake --
-            engine.I.beta    = 1.0;         % Assumed perfect/general (0.99 eta implies PR ~1.0 or specific loss)
+            engine.I.beta    = 0.99;         % Assumed perfect/general (0.99 eta implies PR ~1.0 or specific loss)
             engine.I.eta     = 0.99;
 
             % -- Fan --
-            engine.F.beta    = 1.9;
-            engine.F.eta     = 0.85;        % Fan, LPC & HPC Eff = 0.85
+            engine.F.beta    = 1.35;
+            engine.F.eta     = 0.92;        % Fan, LPC & HPC Eff = 0.85
             
             % -- Low Pressure Compressor --
             % Calculated: OPR (17) / (Fan_PR * HPC_PR) = 17 / (1.9 * 3.5)
-            engine.LPC.beta  = 2.5564;      
-            engine.LPC.eta   = 0.85;
-            
-            % -- High Pressure Compressor --
-            engine.HPC.beta  = 3.5;
-            engine.HPC.eta   = 0.85;
-            
-            % -- Combustor --
-            engine.C.beta    = 0.96;        % From General Characteristics
-            engine.C.eta     = 0.985;
-            engine.C.Qfuel   = 43e6;        % [J/kg]
-            engine.C.Texit   = 1150;        % Turbine Inlet Temp [K]
-            
-            % -- Turbines (Added for completeness based on naming convention) --
-            engine.HPT.eta   = 0.88;
-            engine.LPT.eta   = 0.88;
-            
-            % -- Nozzle & Mechanical --
-            engine.N.eta     = 0.99;        % From General Characteristics
-            engine.eta       = 0.99;        % Mechanical efficiency
-            
-            % -- Flow & Environment --
-            engine.flow.BPR  = 1.62;
-            % Massflow: Core is 90.2. Total = Core * (1 + BPR)
-            engine.flow.mass = 90.2 * (1 + 1.62); 
-            engine.flow.T    = 220;         % Ambient Temp [K]
-            engine.flow.P    = 23842;       % Ambient Pressure [Pa]
-            engine.flow.R    = 287;         
-            engine.flow.M    = 0.78;
-            engine.flow.h    = 10668;
-            
-            % -- Gas Properties --
-            engine.air.gamma = 1.4;
-            engine.air.cp    = 1000;
-            engine.gas.gamma = 1.33;
-            engine.gas.cp    = 1150;
-            engine.gas.LHV   = 43*10^6;
-
-        % =================================================================
-        % CASE 2: LEAP-1B
-        % =================================================================
-        case {'leap-1b', 'leap1b'}
-            engine.name      = 'Leap-1B';
-            
-            % -- Intake --
-            engine.I.beta    = 1.0;         % General
-            engine.I.eta     = 0.99;
-
-            % -- Fan --
-            engine.F.beta    = 1.5;
-            engine.F.eta     = 0.92;        % Fan Eff
-            
-            % -- Low Pressure Compressor --
-            % Calculated: OPR (40) / (Fan_PR * HPC_PR) = 40 / (1.5 * 10)
-            engine.LPC.beta  = 2.6667;
+            engine.LPC.beta  = 4.5;      
             engine.LPC.eta   = 0.92;
             
             % -- High Pressure Compressor --
-            engine.HPC.beta  = 10;
-            engine.HPC.eta   = 0.92;
+            engine.HPC.beta  = 5.5;
+            engine.HPC.eta   = 0.91;
             
             % -- Combustor --
-            engine.C.beta    = 0.96;
+            engine.C.beta    = 0.96;        % From General Characteristics
             engine.C.eta     = 0.995;
-            engine.C.Qfuel   = 43e6;
-            engine.C.Texit   = 1450;
+            engine.C.Texit   = 1600;        % Turbine Inlet Temp [K]
             
-            % -- Turbines --
-            engine.HPT.eta   = 0.92;
-            engine.LPT.eta   = 0.92;
+            % -- Turbines (Added for completeness based on naming convention) --
+            engine.HPT.eta   = 0.91;
+            engine.LPT.eta   = 0.91;
             
             % -- Nozzle & Mechanical --
-            engine.N.eta     = 0.99;
-            engine.eta       = 0.99;
+            engine.N.eta     = 0.99;        % From General Characteristics
+            engine.eta       = 0.995;        % Mechanical efficiency
+            engine.GB.eta    = 0.995;
             
             % -- Flow & Environment --
-            engine.flow.BPR  = 8.6;
-            % Massflow: Core is 50. Total = Core * (1 + BPR)
-            engine.flow.mass = 50 * (1 + 8.6);
-            engine.flow.T    = 220;
-            engine.flow.P    = 23842;
-            engine.flow.R    = 287;
+            engine.flow.BPR  = 12.5;
+            % Massflow: Core is 90.2. Total = Core * (1 + BPR)
+            engine.flow.mass = 220; %kg /s 90.2 * (1 + 1.62); 
+            engine.flow.T    = 216.5;         % Ambient Temp [K]
+            engine.flow.P    = 22632;       % Ambient Pressure [Pa]
+            engine.flow.R    = 287;         
             engine.flow.M    = 0.78;
-            engine.flow.h    = 10668;
-
+            engine.flow.h    = 11000;
+            
             % -- Gas Properties --
             engine.air.gamma = 1.4;
             engine.air.cp    = 1000;
@@ -150,7 +96,8 @@ function engine = initial_data(engineID)
             % -- Nozzle & Mechanical --
             engine.N.eta     = 0.98;        % Specific Validation Data
             engine.eta       = 0.99;
-            
+            engine.GB.eta    = 0.1;
+
             % -- Flow & Environment --
             engine.flow.BPR  = 12;
             engine.flow.mass = 173;         % Given as Total flow
