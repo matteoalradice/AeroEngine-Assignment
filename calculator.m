e41b5c70f3125d34a5f7210d15dc2258b373f90b
matteoalradice <matteo.al.radice@gmail.com>
2025-11-30 15:37:33 +0100

    typo

--- calculator.m
+++ calculator.m
@@ -115,7 +115,7 @@ T8_ii = Tg * (Pg / P_amb)^((1 - gamma_g) / gamma_g);
 
 Wgg = m_core * engine.gas.cp * (Tg - T8_ii);
 
-fprintf('Power required for compression:\t %.2f MW\n\n',m_core * engine.gas.cp * (Tt4 - Tg) / 1e6);
+fprintf('Power required for compression:\t %.2f MW\n\n',W23 / 1e6);
 
 % % % % % % % % % % % % % % Nozzle stage % % % % % % % % % % % % % % % % %
 
